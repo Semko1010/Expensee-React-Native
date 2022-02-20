72a5d9bb88f792f6a40fdbccbddff19637b7c306
Semko1010 <semir01020@gmail.com>
2022-02-20 20:58:29 +0100

    styling the Home Component

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { NativeRouter, Routes, Route, Link } from "react-router-native";
+import RegisterUser from "./Component/Fetch/RegisterUser";
+import Login from "./Component/Login/Login";
+import Home from "./Component/Home/Home";
+import { useState, useEffect } from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NativeRouter>
+			<View style={styles.container}>
+				<Routes>
+					<Route exact path='/' element={<Home />} />
+					<Route path='/register' element={<RegisterUser />} />
+				</Routes>
+			</View>
+		</NativeRouter>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
